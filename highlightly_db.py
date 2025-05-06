76c04e47c40c2009d252b8ce3f5cdab7db94f54e
rflzd <rflzd@yandex.ru>
2025-05-06 07:12:11 +0400

    Boolean added

--- highlightly_db.py
+++ highlightly_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from config import DATABASE_URL  # Database URL .env-dən alınır
